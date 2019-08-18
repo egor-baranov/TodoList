fe5ba75938852d6275985cd843c3409e57851a13
egorbaranov <enterskylife@gmail.com>
2019-08-18 20:32:27 +0300

    :tada:

--- app/src/main/java/com/egorbaranov/todolist/MainActivity.kt
+++ app/src/main/java/com/egorbaranov/todolist/MainActivity.kt
@@ -1,7 +1,30 @@
 package com.egorbaranov.todolist
 
+import android.os.Bundle
 import android.app.Activity
+import android.view.View
+import android.widget.LinearLayout
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import kotlinx.android.synthetic.main.bottom_sheet.*
+
 
 class MainActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
 
+        actionBar?.setBackgroundDrawable(resources.getDrawable(R.color.darkThemeForeground))
+
+        val bottomSheetBehavior: BottomSheetBehavior<LinearLayout> = BottomSheetBehavior.from(bottom_sheet)
+//        bottomSheetBehavior.state = BottomSheetBehavior.STATE_COLLAPSED
+//        bottomSheetBehavior.state = BottomSheetBehavior.STATE_EXPANDED
+//        bottomSheetBehavior.state = BottomSheetBehavior.STATE_HIDDEN
+
+        bottomSheetBehavior.isHideable = false
+
+        bottomSheetBehavior.setBottomSheetCallback(object : BottomSheetBehavior.BottomSheetCallback() {
+            override fun onStateChanged(bottomSheet: View, newState: Int) { }
+            override fun onSlide(bottomSheet: View, slideOffset: Float) { }
+        })
+    }
 }
